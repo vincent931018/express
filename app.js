6f0c81ee0b0277ed66f2f275878a034d9a0302e2
caowencheng <caowencheng@tydic.com>
2016-10-21 15:42:36 +0800

    添加连接maogodb 显示maogodb中数据

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var index = require('./routes/index');
+var mongodb = require('mongodb');
+var router = express.Router();
 
 var app = express();
 
@@ -14,7 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+app.get('/',index);
+
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -55,6 +57,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+
+var server = new mongodb.Server('localhost',27017,{auto_reconnect:true});
+var db = new mongodb.Db("express",server,{safe:false});
+var dataList = '';
+
+//打开数据库连接
+db.open(function(err,db){
+    if(err){
+        console.log(err);
+        return false;
+    }
+    else
+        console.log('connect!');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+
+        //连接到collection
+        db.collection('cwc', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);//错误，返回 err 信息
+            }else{
+                collection.find().toArray(function(err,docs){
+                dataList = docs[0];
+                console.log(dataList);
+                });
+            }
+        });
+        res.render('index',{dataList:dataList.name})
 });
 
 module.exports = router;
